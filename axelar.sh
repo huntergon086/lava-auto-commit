ecc57c1cc095b2b0b56662f0a9e278f6ec4b1fdd
Rizal Sonjaya <116303980+huntergon086@users.noreply.github.com>
2024-05-05 23:05:01 +0700

    Update axelar.sh

--- axelar.sh
+++ axelar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://tm.axelar.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://tm.axelar.lava.build/lava-referer-33a0f030-85f3-4b34-a974-3d32165e3796/"
 
 OUTPUT_DIR="logs"
 
